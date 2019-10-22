1bb1ef046be34aece5b6c3a593bae113bf918e70
matus <matuskoklemo@gmail.com>
2019-10-22 20:08:19 +0200

    Pridana MAC kontrola integrity (este to prepisem na prehladnejsi kod)

--- src/main/CryptoUtils.java
+++ src/main/CryptoUtils.java
@@ -1,22 +1,16 @@
 package main;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class CryptoUtils
 {
-
-
     private static final String ALGORITHM="AES";
     private static final String TRANSFORMATION="AES";
 
@@ -38,10 +32,29 @@ public class CryptoUtils
             cipher.init(cipherMode, secretKey);
             FileInputStream inputStream = new FileInputStream(inputFile);
             byte[] inputBytes = new byte[(int)inputFile.length()];
+
+            // Output stream
             inputStream.read(inputBytes);
+
+            if (cipherMode == Cipher.DECRYPT_MODE && !checkMac(inputFile, inputBytes, key)) {
+                // Throw manipulated file exception
+                throw new IOException("File manipulated");
+            }
+
+            if (cipherMode == Cipher.DECRYPT_MODE) {
+                inputBytes = Arrays.copyOfRange(inputBytes, 0, inputBytes.length - 32);
+            }
             byte[] outputBytes = cipher.doFinal(inputBytes);
+
             FileOutputStream outputStream = new FileOutputStream(outputFile);
-            outputStream.write(outputBytes);
+//            outputStream.write(outputBytes);
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            output.write(outputBytes);
+            if (cipherMode == Cipher.ENCRYPT_MODE) {
+                output.write(getMac(outputStream, outputBytes ,key));
+            }
+
+            outputStream.write(output.toByteArray());
             inputStream.close();
             outputStream.close();
         } catch(
@@ -55,4 +68,44 @@ public class CryptoUtils
             throw new Exception("Errorencrypting/decryptingfile"+ex.getMessage(),ex);
         }
     }
+
+    public static byte[] getMac(FileOutputStream encrypted, byte[] outputBytes ,String key) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+
+        //Append last 16 bytes of mac
+        byte[] authTag = new byte[32];
+
+        Mac mac = Mac.getInstance("HmacSHA1");
+        SecretKeySpec macKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");
+        mac.init(macKey);
+        mac.update(outputBytes);
+        return Arrays.copyOfRange(mac.doFinal(), 0, 32);
+     }
+
+    public static boolean checkMac(File encrypted, byte[] inputBytes, String key) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+        int n = 32;
+        int inputBytesLength = inputBytes.length;
+        byte[] authTag = new byte[n];
+        byte[] inputFileAuthTag = new byte[n];
+        inputFileAuthTag = Arrays.copyOfRange(inputBytes,inputBytes.length - n, inputBytes.length);
+        inputBytes = Arrays.copyOfRange(inputBytes, 0, inputBytesLength - n);
+
+        Mac mac = Mac.getInstance("HmacSHA1");
+        SecretKeySpec macKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");
+
+        mac.init(macKey);
+        mac.update(inputBytes);
+        authTag =  Arrays.copyOfRange(mac.doFinal(), 0, 32);
+
+        // Update array length
+        inputBytesLength = inputBytes.length;
+
+        for(int i = 0; i < n; i++) {
+            if (inputFileAuthTag[i] != authTag[i]) {
+                // Unequal bytes
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
